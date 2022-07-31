0ed1b88889c4e8c9d3acbb75eae51e0d5509328b
nassirou <nassirou9993@gmail.com>
2022-07-31 06:55:06 +0200

    end of the navabar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        brightRed: 'hsl(12,88%,59%)',
+        brightRedLight: 'hsl(12,88%,95%)',
+        brightRedSuplight: 'hsl(12,88%,95%)',
+        darkBlue: 'hsl(228,39%,23%)',
+        darkgrayishBlue: 'hsl(223,12%,13%)',
+        veryDarkBlue: 'hsl(13,100%,96%)',
+        veryPaleRed: 'hsl(13,100%,96%)',
+        veryLightGray: 'hsl(0,0%,98%)',
+      }
+    },
   },
   plugins: [],
 }
